fd2b08e4866f3e59f1f2e769ffbb7125e68ea95f
AradTenenbaum <arad.tenen@gmail.com>
2023-11-12 11:50:31 +0200

    text fix

--- start-dev.bat
+++ start-dev.bat
@@ -2,11 +2,11 @@
 
 REM Navigate to backend directory and install dependencies
 cd "backend"
-echo Installing backend dependencies...
+echo Installing backend dependencies and running the app...
 start cmd /k "npm install && echo Starting backend... && npm run dev"
 
 REM Navigate to frontend directory and install dependencies
 cd ".."
 cd "frontend"
-echo Installing frontend dependencies...
+echo Installing frontend dependencies and running the app...
 start cmd /k "npm install && echo Starting frontend... && npm start"
